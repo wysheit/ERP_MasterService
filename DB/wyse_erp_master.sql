203b6aeb19f62a78b4ace2b1cdec6c91044884bc
Tharindu Chathuranga <tharindunc7@gmail.com>
2020-12-17 09:24:11 +0530

    DB Update

--- DB/wyse_erp_master.sql
+++ DB/wyse_erp_master.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 16, 2020 at 05:52 AM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.3
+-- Generation Time: Dec 17, 2020 at 04:51 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -66,9 +65,9 @@ CREATE TABLE `items` (
   `id` int(11) NOT NULL,
   `item_code` varchar(50) NOT NULL,
   `item_name` varchar(50) NOT NULL,
-  `item_description` text DEFAULT NULL,
   `unit_price` float DEFAULT NULL,
-  `category_code` varchar(50) DEFAULT NULL,
+  `item_description` text DEFAULT NULL,
+  `category_code` varchar(50) NOT NULL,
   `is_active` tinyint(4) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
@@ -79,8 +78,9 @@ CREATE TABLE `items` (
 -- Dumping data for table `items`
 --
 
-INSERT INTO `items` (`id`, `item_code`, `item_name`, `item_description`, `unit_price`, `category_code`, `is_active`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(2, 'ITM-0000000001', 'iphone 12', 'wferge', 5000, 'CAT-0000000003', NULL, '2020-12-16 10:20:53', '2020-12-16 10:20:53', NULL);
+INSERT INTO `items` (`id`, `item_code`, `item_name`, `unit_price`, `item_description`, `category_code`, `is_active`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(2, 'ITM-0000000001', 'iPhone 11', 170000, 'Apple iPhone 11', 'CAT-0000000002', NULL, '2020-12-15 13:17:44', '2020-12-15 13:17:44', NULL),
+(3, 'ITM-0000000002', 'iPhone 12', 210000, '', 'CAT-0000000002', NULL, '2020-12-16 03:43:11', '2020-12-16 03:43:11', NULL);
 
 -- --------------------------------------------------------
 
@@ -144,7 +144,7 @@ ALTER TABLE `employees`
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `item_categories`
